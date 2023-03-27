de208b239ee7f219d2abeb14dd57a0e6f3f81b6b
Tushar Khangar <tusharkhangar7490@gmail.com>
2023-03-27 23:22:46 +0530

    Updated the routes from the homepage and their components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () =>(
+  <div>
+      <h1>HatsPage</h1>
+      <Link to='/topics'>topics</Link>
+  </div>
+)
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Routes>
+      {/* <Route exact path='/' component= {HomePage}/>
+      <Route exact path='/hats' component= {HatsPage}/> */}
+      <Route path="/" element={ <HomePage/> } />
+      <Route path="/hats" element={ <HatsPage/> } />
+      </Routes>
     </div>
   );
 }
