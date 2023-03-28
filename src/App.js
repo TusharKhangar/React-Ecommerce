0264cb3e93ac25751289404b31b8ef483c0f9336
Tushar Khangar <tusharkhangar7490@gmail.com>
2023-03-28 21:45:33 +0530

    routes , navigation path , collection of objects

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import './App.css';
-import { Routes, Route, Link} from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () =>(
-  <div>
-      <h1>HatsPage</h1>
-      <Link to='/topics'>topics</Link>
-  </div>
-)
+
 function App() {
   return (
     <div >
       <Routes>
       {/* <Route exact path='/' component= {HomePage}/>
       <Route exact path='/hats' component= {HatsPage}/> */}
-      <Route path="/" element={ <HomePage/> } />
-      <Route path="/hats" element={ <HatsPage/> } />
+      <Route path='/' element={ <HomePage/> } />
+      <Route path='/shop' element={ <ShopPage/> } />
       </Routes>
     </div>
   );
