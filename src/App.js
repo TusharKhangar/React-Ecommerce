84ae9f581b1a7764fe2b32e6f9fd1a42a1b168cb
Tushar Khangar <tusharkhangar7490@gmail.com>
2023-03-29 13:31:46 +0530

    we Added header page and the logo

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import { Routes, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div >
+      <Header/>
       <Routes>
       {/* <Route exact path='/' component= {HomePage}/>
       <Route exact path='/hats' component= {HatsPage}/> */}
-      <Route path='/' element={ <HomePage/> } />
+      <Route exact path='/' element={ <HomePage/> } />
       <Route path='/shop' element={ <ShopPage/> } />
       </Routes>
     </div>
